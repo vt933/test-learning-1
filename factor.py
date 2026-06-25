47159d73af62233e44c6fa342591b693ad28d70d
vt933 <vthapliyal564@gmail.com>
2026-06-25 19:54:47 +0530

    just a syntax change

--- factor.py
+++ factor.py
@@ -1,6 +1,6 @@
 number_to_factor=int(input("enter the number you would like to factor"))
 #we take i as an element of range 2,number_to_factor here when i is in that range 
-for i in range(2,number_to_factor
+for i in range(2,number_to_factor)
 
     x=number_to_factor%i
     if x==0
