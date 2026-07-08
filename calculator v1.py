60f2c143013638245d96b0f345dc871b61b9ec06
vt933 <vthapliyal564@gmail.com>
2026-07-08 22:55:28 +0530

    Refactor calculator to use functions and menu
    Refactored calculator to use functions for operations and added a menu for user interaction.

--- calculator v1.py
+++ calculator v1.py
@@ -1,13 +1,47 @@
-x=int(input("enter first no:-"))
-y=int(input("enter second no:-"))
-opertator=input("enter + or add - or minus divide or / mutiply or *:-")
-if opertator=="+" or  opertator=="addition":
-    print(x+y)
-elif opertator=="-" or   opertator=="minus":
-    print (x-y)
-elif opertator=="*" or  opertator=="multiply":
-    print (x*y)
-elif opertator=="/"or  opertator=="divide":
- print(x/y)
-else:
-  print("invalid operator")
+#calculator.py
+
+menu_funtion=("1.addition",
+              "2.Subtraction",
+              "3.Multiplication",
+              "4.Division",
+              "5.exit",
+)
+
+def addition(a,b):
+    return a+b
+
+def subtraction(a,b):
+    return a-b
+
+def division(a,b):
+    if b==0:
+       return("number is not divisble by zero")
+    else:
+       return a/b
+
+def multiplication(a,b):
+    return a*b
+while True:
+ for item in menu_funtion:
+  print(item)
+ user_input=int(input("enter user input as(1,2,3,4,or5)"))
+ if user_input==5:
+    break
+ a=float(input("enter the number you want to perform operation on"))
+ b=float(input("enter the number you want to perform operation on"))
+ if user_input==1:
+    result=addition(a,b)
+    print(result)
+
+ elif user_input==2:
+    r2=subtraction(a,b)
+    print(r2)
+ elif user_input==3:
+    r3=multiplication(a,b)
+    print(r3)
+
+ elif user_input==4:
+    r4=division(a,b)
+    print(r4)
+ else:
+    print(f"invalid input {user_input}") 
